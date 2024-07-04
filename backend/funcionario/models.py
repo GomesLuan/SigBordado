9f01eff8cf33b6dc006bc663a6c623882f8d737e
GomesLuan <luan.araujo.gom@gmail.com>
2024-07-04 09:13:31 -0300

    feat: add funcionario #45

---
+++ backend/funcionario/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Funcionario(models.Model):
+    cod = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11)
+    senha = models.CharField(max_length=50)
+    rg = models.CharField(max_length=10)
+    email = models.CharField(max_length=150)
+    telefone = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=250) 
