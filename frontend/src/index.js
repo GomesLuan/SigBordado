587e070b4293de6ba31d09db733ddf307bb175a1
Victor Ryan <vitorsilva3612@gmail.com>
2024-07-17 10:19:21 -0300

    feat: botao ir para rota funcionario #46

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <Routes>
-      <Route path="/funcionario" element={<App />} />
-      {/* Adicione outras rotas aqui */}
-    </Routes>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/funcionario">Ir para Funcionário</Link>
+          </li>
+          {/* Adicione mais itens de menu conforme necessário */}
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/funcionario" element={<App />} />
+        {/* Adicione outras rotas aqui */}
+      </Routes>
+    </div>
   </Router>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
