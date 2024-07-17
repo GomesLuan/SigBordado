4bc970da1c7ff414a6f2addf71d9a49486989a05
Victor Ryan <vitorsilva3612@gmail.com>
2024-07-17 10:20:02 -0300

    feat: listagem de operacoes na coluna esquerda # 46

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
+  // Função que será chamada ao clicar no texto
+  const handleClick = (text) => {
+    alert(`Você clicou em: ${text}`);
+    // Você pode executar qualquer função aqui
+  };
+
   return (
     <div className="App">
       <header className="header">Header Fixo</header>
       <div className="container">
-        <div className="column column-left"></div>
+        <div className="column column-left">
+          <p onClick={() => handleClick('Texto 1')}>Texto 1</p>
+          <p onClick={() => handleClick('Texto 2')}>Texto 2</p>
+          <p onClick={() => handleClick('Texto 3')}>Texto 3</p>
+          {/* Adicione mais textos clicáveis conforme necessário */}
+        </div>
         <div className="column column-right"></div>
       </div>
     </div>
