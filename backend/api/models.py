8dac9c84287d5da363cdd78a11ef927dea48c6fc
GomesLuan <luan.araujo.gom@gmail.com>
2024-08-08 20:19:42 -0300

    feat: add models Produto e MaterialProduto #67

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,3 +22,13 @@ class Material(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+class Produto(models.Model):
+    cod = models.AutoField(primary_key=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.CharField(max_length=250)
+
+class MaterialProduto(models.Model):
+    codProduto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    codMaterial = models.ForeignKey(Material, on_delete=models.CASCADE)
+    quantUsa = models.FloatField()
